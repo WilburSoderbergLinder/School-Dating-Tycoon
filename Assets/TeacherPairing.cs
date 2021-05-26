ea280d7e5df652237d045d19041dae47faf311db
WonderfulK-JGithub <70464858+WonderfulK-JGithub@users.noreply.github.com>
2021-05-26 14:24:05 +0200

    TeacherTraits

---
+++ Assets/TeacherPairing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacherPairing : MonoBehaviour
+{
+    TeacherTraits pairedTeacher = null;
+
+   public void Pair(TeacherTraits teacher)
+   {
+        if(pairedTeacher == null)
+        {
+            pairedTeacher = teacher;
+        }
+        else
+        {
+            int thingsInCommon = 0;
+
+            for (int i = 0; i < teacher.traits.Length; i++)
+            {
+                if (teacher.traits[i] == pairedTeacher.traits[i]) thingsInCommon++;
+            }
+        }
+   }
+}
