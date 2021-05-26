638c0b3a135fa3b02b39b137ca1883039cddc0a7
WilburSoderbergLinder <79506125+WilburSoderbergLinder@users.noreply.github.com>
2021-05-26 14:06:30 +0100

    GlobalaPengar

---
+++ Assets/EkonomiScripts/ObjectStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectStats : MonoBehaviour
+{
+    public int Cost;
+    
+}
---
+++ Assets/EkonomiScripts/Saves.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class Saves : MonoBehaviour
+{
+    public static Saves Instance { get; private set; }
+
+    public int money = 0;
+    public int students = 0;
+    public int income = 100;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+}
