0138e46231e737d71adfb12763e73b6f19346627
Strutsiboi <melker.johansson@elev.ga.lbs.se>
2021-05-26 14:14:51 +0200

    Köpa Saker
    Jag har gjort att när man klickar på en "ConstruktionArea" så får man en meny där man kan köpa olika saker. När man valt så raderas "ConstruktionArea" och saken man köpte spawnas. Jag kommer dock behöva fixa det mer.
    Melker J

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+    public GameObject BuildMenu;
+    public GameObject ConstructionArea;
+    public GameObject TestObject;
+    void Start()
+    {
+        BuildMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero);
+
+            if (hit && hit.transform.tag == "ConstructionArea")
+            {
+                Debug.Log(hit.transform.name);
+                BuildMenu.SetActive(true);
+            }
+
+        }
+    }
+
+    public void build ()
+    {
+        Destroy(ConstructionArea);
+        BuildMenu.SetActive(false);
+        Instantiate(TestObject);
+    }
+}
