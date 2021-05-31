ef63f97689d8c5ca8c235e7274ff5edbec9b7b15
GustavJoha <79506494+GustavJoha@users.noreply.github.com>
2021-05-31 22:31:39 +0200

    scriptet som lärare kommer använda

---
+++ Assets/teacher.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teacher : MonoBehaviour
+{
+    // finns för tillfället inte särskillt mycket att tilläga på lärar skriptet, behöver tid system och pathfinding för att kunna implementera dem flesta funktionerna.
+
+    // variabler för stat ändringar kommer att finnas i ett annat skript. 
+}
