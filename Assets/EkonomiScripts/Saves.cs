0eb1fa07fb7690db6fe13b53c82860039ddb056b
WilburSoderbergLinder <79506125+WilburSoderbergLinder@users.noreply.github.com>
2021-05-31 23:21:34 +0100

    TimeSystem

--- Assets/EkonomiScripts/Saves.cs
+++ Assets/EkonomiScripts/Saves.cs
@@ -7,11 +7,13 @@ public class Saves : MonoBehaviour
 {
     public static Saves Instance { get; private set; }
 
+
+    //Lista över värden som ska sparas över scripts.
     public int money = 0;
     public int students = 0;
     public int income = 100;
 
-    private void Awake()
+    private void Awake() //Gör scriptet till en Singleton
     {
         if (Instance != null && Instance != this)
         {
