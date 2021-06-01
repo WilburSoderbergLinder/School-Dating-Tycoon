e584a75dd0a9388e232af064ac5a93dd35a4b34d
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2021-06-01 23:31:50 +0200

    Meny

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+    public AudioSource audioSource;
+    void Start()
+    {
+        //Musiken ska automatiskt börja spela -Saga
+        audioSource.Play();
+    }
+    public void StopMusic()
+    {
+        //När man trycker på knappen ska det här uppfyllas, musiken ska sluta spela -Saga
+        audioSource.Stop();
+        print("Music Stop"); //För att försäkra
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    public void Quit()
+    {
+        Quit();
+    }
+
+}
