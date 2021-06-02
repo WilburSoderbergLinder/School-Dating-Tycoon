d86c4b32132cc33a1221d61a39395b5d49a077e4
WilburSoderbergLinder <79506125+WilburSoderbergLinder@users.noreply.github.com>
2021-06-02 12:48:52 +0200

    Nyttlån

---
+++ Assets/EkonomiScripts/Loan.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework.Constraints;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loan : MonoBehaviour
+{
+    public int loanSize;
+    int loan;
+    public float interest;
+    public float mortgageRate;
+
+
+
+    public void PayMortgage()
+    {
+        
+        loan -= Mathf.FloorToInt(loanSize * mortgageRate);
+        Saves.Instance.money -= Mathf.FloorToInt(loanSize * mortgageRate);
+
+
+        if (loan <= 0)
+        {
+            loanSize = 0;
+            loan = 0;
+            interest = 0;
+            mortgageRate = 0;
+            Destroy(this);
+        }
+    }
+    public void SetLoan()
+    {
+        loan = Mathf.FloorToInt(loanSize*interest);
+    }
+}
--- Assets/EkonomiScripts/TimeCounter.cs
+++ Assets/EkonomiScripts/TimeCounter.cs
@@ -2,51 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 //Kod av Daniel
+[RequireComponent(typeof(TimeSystem))]
 public class TimeCounter : MonoBehaviour
 {
+    Loan[] loans = new Loan[5];
 
-    float minutes = 0;
-    int hours = 0;
-    int days = 0;
-    int weeks = 0;
-    int months = 0;
-    int years = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float minutes = 0;
+    public int hours = 0;
+    public int days = 0;
+    public int weeks = 0;
+    public int months = 0;
+    public int years = 0;
 
     // Update is called once per frame
     void Update()
     {
-        minutes += Time.deltaTime; //Time.deltaTime kommer att multipliceras med en variabel som kommer från ett annat script, jag har inte tillgång till den än. - Daniel
+        minutes += Time.deltaTime * TimeSystem.Instance.timeScale; //Time.deltaTime kommer att multipliceras med en variabel som kommer från ett annat script, jag har inte tillgång till den än. - Daniel
 
         if (minutes >= 60) //Lägger till ett värde på "hours" om "minutes" är lika med eller större än 60. - Daniel
         {
             hours++;
-            print(hours);
             minutes -= 60;
         }
 
         if (hours >= 24) //Lägger till ett värde på "days" om "hours" är lika med eller större än 24. - Daniel
         {
             days++;
-            print(days);
             hours = 0;
         }
 
         if (days >= 7) //Lägger till ett värde på "weeks" om "days" är lika med eller större än 7. - Daniel
         {
             weeks++;
-            print(weeks);
             days = 0;
         }
 
         if (weeks >= 4) //Lägger till ett värde på "months" om "weeks" är lika med eller större än 4. - Daniel
         {
             months++;
-            print(months);
             weeks = 0;
             NewMonth();
         }
@@ -54,7 +47,6 @@ public class TimeCounter : MonoBehaviour
         if (months >= 12) //Lägger till ett värde på "years" om "months" är lika med eller större än 12. - Daniel
         {
             years++;
-            print(years);
             months = 0;
         }
 
@@ -63,5 +55,40 @@ public class TimeCounter : MonoBehaviour
     public void NewMonth()
     {
         Saves.Instance.money += Saves.Instance.students * Saves.Instance.income;
+        foreach (Loan item in loans)
+        {
+            item.PayMortgage();
+        }
+    }
+
+    public void UINewLoan(int loanSize)
+    {
+        NewLoan(loanSize, 1.1f, 0.1f);
+    }
+
+    public void NewLoan(int loanSize, float interest, float mortgageRate)
+    {
+
+        foreach (Loan i in loans)
+        {
+            
+        }
+        for (int i = 0; i < loans.Length; i++)
+        {
+            if (loans[i] == null)
+            {
+                //tomt
+                loans[i] = gameObject.AddComponent<Loan>();
+                loans[i].loanSize = loanSize;
+                Saves.Instance.money += loanSize;
+                loans[i].interest = interest;
+                loans[i].mortgageRate = mortgageRate;
+                loans[i].SetLoan();
+                print("nytt Lån");
+                return;
+            }
+        }
+        print("för många");
+        //För många
     }
 }
