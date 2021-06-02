dc8c99adc1bcb79642739dd2035bd86f2fcd3d5a
Max <79506271+max-aug@users.noreply.github.com>
2021-06-02 13:06:25 +0200

    kommentarer

--- Assets/Pathfinding/PathfinderManagerScript.cs
+++ Assets/Pathfinding/PathfinderManagerScript.cs
@@ -6,6 +6,7 @@ public class PathfinderManagerScript : MonoBehaviour
     public static PathfinderManagerScript instance { get; private set; }
 
     //Skapar en ny pathfindingpunkt som lärarna har möjlighet att gå till - Max
+    //Det borde göras vid dörrar, i klassrummen och sånt, överallt där lärarna ska kunna gå
     public void AddPathfindingPoint(Vector2 position)
     {
         Transform newG = new GameObject().transform;
